a1467ae8c77692d4210cc2ce6ac2c97bfb9d8ec6
trevormheath <trevormheath@gmail.com>
2024-09-30 19:19:32 -0600

    Initial Commit

---
+++ MyCalculator/MyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCalculatorApp.swift
+//  MyCalculator
+//
+//  Created by IS 543 on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
